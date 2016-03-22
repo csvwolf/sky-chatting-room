a27134cc23245a95669e4f113c1e1fd7616cc244
SkyAo <csvwolf@qq.com>
2016-03-22 20:21:40 +0800

    初步实现多线程处理,不会阻塞

--- client.py
+++ client.py
@@ -2,16 +2,45 @@
 # -*- coding:utf-8 -*-
 
 import socket
+import threading
 
 s = socket.socket()
 
 host = socket.gethostname()
 port = 1234
-
+status = True
 s.connect((host, port))
-print s.recv(1024)
-s.sendall('Hello World')
-while 1:
-    word = raw_input('>')
-    s.sendall(word)
-    print s.recv(1024)
+
+class inputThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while 1:
+            if (status):
+                word = raw_input('>')
+                s.sendall(word)
+
+class receiveThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while 1:
+            print s.recv(1024)
+
+if __name__ == '__main__':
+    input = inputThread()
+    receiver = receiveThread()
+
+    input.start()
+    receiver.start()
+
+    print 'Client Start'
+
+# print s.recv(1024)
+# s.sendall('Hello World')
+# while 1:
+#     word = raw_input('>')
+#     s.sendall(word)
+#     print s.recv(1024)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import socket
+import threading
 
 s = socket.socket()
 
@@ -11,24 +12,56 @@ s.bind((host, port))
 lists = []
 s.listen(5)
 
-while True:
-    c, addr = s.accept()
-    lists.append({'client': c, 'address': addr})
-    print lists
-    print 'Got connection from', addr
-    c.send('Thank you for connecting')
-    for client in lists:
-        msg = client['client'].recv(1024)
-        if msg:
-            print msg
-            for receiver in lists:
-                if receiver != client:
-                    receiver['client'].sendall(msg)
-    #word = raw_input('>')
-    #for c in lists:
-        #c['client'].sendall(word)
+class scanThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
 
-for client in lists:
-    client['client'].close()
+    def run(self):
+        while True:
+            c, addr = s.accept()
+            lists.append({'client': c, 'address': addr})
+            receiveThread(c, addr).start()
+            print lists
+            print 'Got connection from', addr
+            c.send('Thank you for connecting')
+
+class receiveThread(threading.Thread):
+    def __init__(self, client, address):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+
+    def run(self):
+        while True:
+            msg = self.client.recv(1024)
+            if msg:
+                print msg
+                for receiver in lists:
+                    if receiver['address'] != self.address:
+                        receiver['client'].sendall(msg)
+
+# while True:
+#     c, addr = s.accept()
+#     lists.append({'client': c, 'address': addr})
+#     print lists
+#     print 'Got connection from', addr
+#     c.send('Thank you for connecting')
+#     for client in lists:
+#         msg = client['client'].recv(1024)
+#         if msg:
+#             print msg
+#             for receiver in lists:
+#                 if receiver != client:
+#                     receiver['client'].sendall(msg)
+#     #word = raw_input('>')
+#     #for c in lists:
+#         #c['client'].sendall(word)
+
+if __name__ == '__main__':
+    scanner = scanThread()
+    scanner.start()
+
+# for client in lists:
+#     client['client'].close()
 
 
