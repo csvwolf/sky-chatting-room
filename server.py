259e415af42a23182562f6ae2cac45750a4bc612
SkyAo <csvwolf@qq.com>
2016-03-24 21:37:31 +0800

    读者写者模型

--- server.py
+++ server.py
@@ -21,6 +21,7 @@ class scanThread(threading.Thread):
         while True:
             c, addr = s.accept()
             receiver = receiveThread(c, addr)
+            receiver.setDaemon(True)
             threadLock.acquire()
             lists.append(receiver)
             threadLock.release()
@@ -30,6 +31,8 @@ class scanThread(threading.Thread):
             # c.send('Thank you for connecting')
 
 class receiveThread(threading.Thread):
+    counter = 0
+
     def __init__(self, client, address):
         threading.Thread.__init__(self)
         self.client = client
@@ -44,7 +47,11 @@ class receiveThread(threading.Thread):
             if msg:
                 print msg
                 if msg['type'] == 'message':
-                    threadLock.acquire()
+                    mutex.acquire()
+                    if receiveThread.counter == 0:
+                        threadLock.acquire()
+                    receiveThread.counter += 1
+                    mutex.release()
                     for receiver in lists:
                         if receiver.address != self.address:
                             print self.address, ' -> ', receiver.address
@@ -52,7 +59,11 @@ class receiveThread(threading.Thread):
                                 send_msg(receiver.client, repr(msg).encode('utf8'))
                             except Exception, e:
                                 lists.remove(receiver)
-                    threadLock.release()
+                    mutex.acquire()
+                    receiveThread.counter -= 1
+                    if receiveThread.counter == 0:
+                        threadLock.release()
+                    mutex.release()
                 elif msg['type'] == 'command':
                     if msg['content'] == 'quit':
                         threadLock.acquire()
@@ -79,9 +90,10 @@ class receiveThread(threading.Thread):
 
 if __name__ == '__main__':
     scanner = scanThread()
+    scanner.setDaemon(True)
     scanner.start()
     threadLock = threading.Lock()
-    mutex = threadLock.Lock()
+    mutex = threading.Lock()
 
     for client in lists:
         client.client.close()
